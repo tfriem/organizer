8dde126f2b71c246aba9931ff2b105eaaea0db74
Tobias Friemel <tfriem@gmail.com>
2018-05-13 10:17:14 +0200

    Remove default content

---
+++ lib/work_time_tracker.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WorkTimeTrackerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello World!'),
+      appBar: AppBar(
+        title: Text('Work Time Tracker'),
+      ),
+    );
+  }
+}
