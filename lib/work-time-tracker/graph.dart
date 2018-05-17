8488070fd19143be023189594e72c00b6287d9a6
Tobias Friemel <tfriem@gmail.com>
2018-05-17 20:14:38 +0200

    Add tab bar with a first graph screen

---
+++ lib/work-time-tracker/graph.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import '../app/model.dart';
+import '../core/date.dart';
+import 'model.dart';
+
+class Graph extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, GraphViewModel>(
+        converter: (Store<AppState> store) =>
+            GraphViewModel(store.state.workTimeTracker.bookings),
+        builder: (BuildContext context, GraphViewModel vm) {
+          return charts.TimeSeriesChart(
+            [
+              charts.Series<MapEntry<Date, Booking>, DateTime>(
+                id: 'Start',
+                domainFn: (datum, int index) => datum.key.toDateTime(),
+                measureFn: (datum, int index) =>
+                    datum.value.start.hour * 100 + datum.value.start.minute,
+                data: vm.bookings.entries.toList(),
+              ),
+              charts.Series<MapEntry<Date, Booking>, DateTime>(
+                id: 'End',
+                domainFn: (datum, int index) => datum.key.toDateTime(),
+                measureFn: (datum, int index) =>
+                    datum.value.end.hour * 100 + datum.value.end.minute,
+                data: vm.bookings.entries.toList(),
+              )
+            ],
+            behaviors: [
+              charts.SeriesLegend(position: charts.BehaviorPosition.bottom)
+            ],
+          );
+        });
+  }
+}
+
+class GraphViewModel {
+  final Map<Date, Booking> bookings;
+
+  GraphViewModel(this.bookings);
+}
--- lib/work-time-tracker/screen.dart
+++ lib/work-time-tracker/screen.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 
 import 'booking_detail.dart';
 import 'horizontal_calendar.dart';
+import 'graph.dart';
 
 class WorkTimeTrackerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Work Time Tracker'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(child: BookingDetail()),
-          Container(height: 100.0, child: HorizontalCalendar())
-        ],
-      ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('Work Time Tracker'),
+            bottom: TabBar(
+              tabs: <Widget>[Tab(text: 'Buchen'), Tab(text: 'Auswertung')],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Expanded(child: BookingDetail()),
+                  Container(height: 100.0, child: HorizontalCalendar())
+                ],
+              ),
+              Column(
+                children: <Widget>[Expanded(child: Graph())],
+              )
+            ],
+          )),
     );
   }
 }
